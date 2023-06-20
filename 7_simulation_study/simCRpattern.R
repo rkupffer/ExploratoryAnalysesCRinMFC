cc6d8b2fe07129cf053c902a9d9af08d65bc4038
Rebekka Kupffer <kupffer@uni-landau.de>
2023-06-20 18:56:19 +0200

    add folder an script for the planned sumulation study

---
+++ 7_simulation_study/simCRpattern.R
@@ -0,0 +1,9 @@
+#### simulation and detection of different careless responding pattern  ####
+
+# In this script (4) the different careless and careful responding pattern 
+# will be simulated with varying proportions.
+
+# packages ----
+library(MFCblockInfo)
+library(TirtAutomation)
+library(CRinMFC)
